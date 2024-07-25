922aed82ef72302524268a21206b592e1b74b9b7
Nestor Saenz <nes164@gmail.com>
2024-07-25 15:39:33 -0500

    actualizando los archivos para subir a la nube el tablero

---
+++ grafico_barras_horizontal.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import plotly.express as px
+
+def crear_grafico(df):
+    df = df.groupby('producto')['ingreso_neto'].sum().sort_values(ascending = True).reset_index()
+    
+    # Se crea la grafica
+    fig = px.bar(df.tail(10),
+                 x = 'ingreso_neto',
+                 y = 'producto',
+                 text = 'ingreso_neto',
+              )
+    
+    fig.update_layout(yaxis_title = 'Productos', 
+                      xaxis_title = 'Ingresos ($)', 
+                      showlegend = False,
+                      title={
+                        'text': 'Top de productos que generan mas ingresos ($)',
+                        'y':0.95,
+                        'x':0.43,
+                        'xanchor': 'center',
+                        'yanchor': 'top',
+                        'font': {
+                            'color': '#cfae48',
+                            'size': 22
+                        }
+    })
+    
+    fig.update_traces(texttemplate = '%{text:.3s}',
+                      textfont=dict(color="white"),
+                      #textposition='outside',
+                      marker_color='#003f5c'
+                      
+                      )
+
+    return fig
