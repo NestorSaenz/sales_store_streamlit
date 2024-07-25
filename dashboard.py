fb55a6d5ebe27ac0f3521ce205ca401a8417488e
Nestor Saenz <nes164@gmail.com>
2024-07-25 16:08:04 -0500

    Actualizando la url de la base de datos

--- dashboard.py
+++ dashboard.py
@@ -27,7 +27,8 @@ def formato_metrica(valor, prefijo = ''):
     return f'{prefijo} {valor:.2f}'
 
 # Se abre la base de datos 
-df= pd.read_csv('https://raw.githubusercontent.com/NestorSaenz/sales_dashboard_streamlit/main/df_final.csv')
+df= pd.read_csv('https://raw.githubusercontent.com/NestorSaenz/sales_store_streamlit/main/df_final.csv')
+
 # antes de github agregar raw. y despues de github agregar usercontent, blob se borra
 
 
